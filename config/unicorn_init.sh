e73e4749c998a9c56b039f2cf6c1b079c79a55c4
Hong Kim <honki91@gmail.com>
2012-04-20 20:46:45 -0700

    changed to development for the time being

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -7,7 +7,7 @@ set -e
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/deployer/apps/soobooki/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E development"
 AS_USER=deployer
 action="$1"
 set -u
